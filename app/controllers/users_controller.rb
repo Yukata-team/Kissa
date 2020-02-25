4befc17f42cdd604d764a3c00b445f4e72bf3982
Yugo <yu1931sn@ezweb.ne.jp>
2020-02-25 16:58:49 +0900

    [Add] プロフィール画像(未完成)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,22 @@ class UsersController < ApplicationController
 
   end
 
+  def image_update
+    @user = current_user
+    @user.save(user_image_params)
+    redirect_to user_path(@user), notice: "プロフィール画像を変更しました"
+  end
+
   def ensure_correct_user
     @user = User.find(params[:id])
     if @user.id != current_user.id
       flash[:alert] = "権限がありません"
       redirect_to user_path(current_user.id)
     end
+  end
+
+  private
+  def user_image_params
+    params.require(:user).permit(:profile_image)
   end
 end
