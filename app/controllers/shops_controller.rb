f24149cfa07510bfd68e0f1331858d6ed6ac61bb
Yugo <yu1931sn@ezweb.ne.jp>
2020-03-08 15:52:48 +0900

    [Fixed] 店舗登録時のバグ

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -104,7 +104,7 @@ class ShopsController < ApplicationController
 
   private
   def shop_params
-    params.require(:shop).permit(:name, :branch, :furigana, :station_name, :other_name, :business_hour, :head_image, :postal_code, :prefecture, :city, :street,:postcode, :prefecture_name, :address_city, :address_street, :address_building, shop_images_images: [])
+    params.require(:shop).permit(:name, :branch, :furigana, :station_name, :other_name, :business_hour, :head_image, :postal_code, :prefecture, :city, :street,:postcode, :prefecture_code, :address_city, :address_street, :address_building, shop_images_images: [])
   end
 
   def search_params
@@ -112,7 +112,7 @@ class ShopsController < ApplicationController
   end
 
   def zipedit
-    params.require(:shop).permit(:postcode, :prefecture_name, :address_city, :address_street, :address_building)
+    params.require(:shop).permit(:postcode, :prefecture_code, :address_city, :address_street, :address_building)
   end
 
 end
