03b04faad3237f0480124fc99149780d693a3664
Yugo <yu1931sn@ezweb.ne.jp>
2020-03-22 16:45:26 +0900

    [Add] 複数のワードでの検索機能

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -28,8 +28,15 @@ class Shop < ApplicationRecord
     return User.find(self.user_id)
   end
 
-  # def address
-  #   [address_street, address_city, prefecture_code].compact.join(', ')
+  # scope :by_any_texts, (lambda do |string|
+  #   string.split(' ').inject(self) do |scope, text|
+  #     query = search(name_or_age_or_hobby_or_posts_content_cont_any: text).result
+  #     scope.merge(query)
+  #   end
+  # end)
+  #
+  # def self.ransackable_scopes(_user = nil)
+  #   %i[by_any_texts]
   # end
 
   include JpPrefecture
