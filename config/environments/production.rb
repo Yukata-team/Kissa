58df3901fe862a4bdfe14bb37840454a4e63f770
Yugo <yu1931sn@ezweb.ne.jp>
2020-03-23 01:46:11 +0900

    [Add] メール認証機能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,16 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['GOOGLE_MAIL'], #gmailアドレス
+    :password => ENV['GOOGLE_PASS'], #gmailパスワード
+    :authentication => 'login',
+  }
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
